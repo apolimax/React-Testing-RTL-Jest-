71fc6163f9cb19a5c45b8efd32adc4baa2f653e6
Will Apolinario <willieapolinario@gmail.com>
2022-09-28 10:11:53 -0300

    getByRole, getByLabelText

---
+++ src/components/Application/Application.test.tsx
@@ -0,0 +1,34 @@
+import { render, screen } from "@testing-library/react";
+import Application from "./Application";
+
+describe("App", () => {
+  test("App renders correctly", () => {
+    render(<Application />);
+
+    const pageHeadingElement = screen.getByRole("heading", {
+      level: 1,
+    });
+    const sectionHeadingElement = screen.getByRole("heading", {
+      level: 2,
+    });
+
+    /* const nameElement = screen.getByRole("textbox", { name: "Name" }); */
+    const nameElement = screen.getByLabelText("Name"); // finds the label with the given text and look for the element associated with it
+    const bioElement = screen.getByRole("textbox", { name: "Bio" });
+    const jobLocation = screen.getByRole("combobox");
+    /* const termsElement = screen.getByRole("checkbox"); */
+    const termsElement = screen.getByLabelText(
+      "I agree to the terms and conditions"
+    );
+    const submitButton = screen.getByRole("button");
+
+    expect(pageHeadingElement).toBeInTheDocument();
+    expect(sectionHeadingElement).toBeInTheDocument();
+
+    expect(nameElement).toBeInTheDocument();
+    expect(bioElement).toBeInTheDocument();
+    expect(jobLocation).toBeInTheDocument();
+    expect(termsElement).toBeInTheDocument();
+    expect(submitButton).toBeInTheDocument();
+  });
+});
--- src/components/Greet/Greet.test.tsx
+++ src/components/Greet/Greet.test.tsx
@@ -1,14 +1,18 @@
 import { render, screen } from "@testing-library/react";
 import Greet from "./Greet";
 
-test("Greet renders correctly", () => {
-  render(<Greet />);
-  const textElement = screen.getByText("Hello");
-  expect(textElement).toBeInTheDocument();
+describe("Greet", () => {
+  test("renders correctly", () => {
+    render(<Greet />);
+    const textElement = screen.getByText(/Hello/);
+    expect(textElement).toBeInTheDocument();
+  });
 });
 
-test("Greet renders with a name", () => {
-  render(<Greet name="John" />);
-  const textElement = screen.getByText("Hello John");
-  expect(textElement).toBeInTheDocument();
-});
+/* describe("Nested", () => {
+  test("renders with a name", () => {
+    render(<Greet name="John" />);
+    const textElement = screen.getByText("Hello John");
+    expect(textElement).toBeInTheDocument();
+  });
+}); */
