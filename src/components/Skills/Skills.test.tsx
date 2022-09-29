b201f2721a9af551c21783844a9c70a20ca3a37e
Will Apolinario <willieapolinario@gmail.com>
2022-09-29 10:28:18 -0300

    test husky

--- src/components/Skills/Skills.test.tsx
+++ src/components/Skills/Skills.test.tsx
@@ -1,4 +1,5 @@
-import { render, screen, logRoles } from "@testing-library/react";
+/* import { render, screen, logRoles } from "@testing-library/react"; */
+import { render, screen } from "@testing-library/react";
 import Skills from "./Skills";
 
 describe("Skills", () => {
